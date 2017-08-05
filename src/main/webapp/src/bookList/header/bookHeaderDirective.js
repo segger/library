43fd2d805f73714684042aa2d2cbcafba8be85c0
segger <johanna.lynn.lahti@gmail.com>
2017-08-05 11:39:13 +0200

    Restart webapp

--- src/main/webapp/src/bookList/header/bookHeaderDirective.js
+++
@@ -1,9 +0,0 @@
-import BookHeaderController from './BookHeaderController';
-
-export default function bookHeaderDirective() {
-  return {
-    controller: BookHeaderController,
-    controllerAs: 'bookHeader',
-    template: require('./bookHeader.html'),
-  };
-}
--- src/main/webapp/webpack-server.config.js
+++
@@ -1,9 +0,0 @@
-const makeWebpackConfig = require('./make-webpack-config');
-
-const config = makeWebpackConfig({
-    devServer: true,
-    devtool: 'inline-source-map',
-    debug: true,
-});
-
-module.exports = config;
