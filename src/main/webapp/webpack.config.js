b050203a2f628dddcf1a08f565a0bed0aab62b6a
segger <johanna.lynn.lahti@gmail.com>
2017-08-05 11:55:06 +0200

    Very basic webapp

---
+++ src/main/webapp/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.join(__dirname, 'target'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader'
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
